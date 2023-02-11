2544ff0ff67eb00cd0f1dd16801836e33c4f8aee
Kokoro Ibaragi <kokoro.ibaragi@qrioinc.com>
2023-02-12 07:52:05 +0900

    fix

--- install.sh
+++ install.sh
@@ -1,31 +1,33 @@
 # vi:ft=bash:
 if test `uname` = "Darwin"; then
-  ln -s ./osx/zshenv $HOME/.zshenv
-  ln -s ./osx/zshrc $HOME/.zshrc
+  ln -sf osx/zshenv $HOME/.zshenv
+  ln -sf osx/zshrc $HOME/.zshrc
 
-  ln -s ./osx/Brewfile $HOME/.Brewfile
+  ln -sf osx/Brewfile $HOME/.Brewfile
   if where brew &>/dev/null; then
     brew bundle --global
   else
     mkdir -p $HOME/.local/homebrew
-    curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C $HOME/.local/homebrew
+    git clone https://github.com/Homebrew/brew $HOME/.local/homebrew
     eval `$HOME/.local/homebrew shellenv`
+    brew update --force --quiet
+    chmod -R go-w "$(brew --prefix)/share/zsh"
     brew bundle --global
   fi
 fi
 
 if test `uname` = "Linux"; then
-  ln -s ./linux/zshenv $HOME/.zshenv
-  ln -s ./linux/zshrc $HOME/.zshrc
+  ln -sf linux/zshenv $HOME/.zshenv
+  ln -sf linux/zshrc $HOME/.zshrc
 
   if [ ! -e "$HOME/.config/fontconfig" ]; then
     mkdir -p $HOME/.config/fontconfig
   fi
-  ln -s ./linux/000-fontstweak-ezfc.conf $HOME/.config/fontconfig/
+  ln -sf linux/000-fontstweak-ezfc.conf $HOME/.config/fontconfig/
 fi
 
-ln -s ./share/gitconfig $HOME/.gitconfig
-ln -s ./share/init.vim $HOME/.config/nvim/
-ln -s ./share/antigen.conf.zsh $HOME/.antigen.conf.zsh
+ln -sf share/gitconfig $HOME/.gitconfig
+ln -sf share/init.vim $HOME/.config/nvim/
+ln -sf share/antigen.conf.zsh $HOME/.antigen.conf.zsh
 
 exit
